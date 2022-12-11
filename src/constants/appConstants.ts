afa44543186f36bc245d1d6b57373fc74f36bb7b
Wisdom Ossai <ossaiwisdom48@gmail.com>
2022-12-11 19:15:12 +0100

    import fornt

--- src/constants/appConstants.ts
+++ src/constants/appConstants.ts
@@ -26,3 +26,55 @@ export const CERTIFICATE_STEPS = [
   { value: 3, label: "Upload List", path: paths.CERTIFICATES_UPLOAD_LIST },
   { value: 4, label: "Preview", path: paths.CERTIFICATES_PREVIEW },
 ];
+
+export const FONTS = [
+  { label: "Dosis", value: "'Dosis', sans-serif" },
+  {
+    label: "EB Garamond",
+    value: "'EB Garamond', serif",
+  },
+  {
+    label: "Fira Sans",
+    value: "'Fira Sans', sans-serif",
+  },
+  {
+    label: "Lato",
+    value: "'Lato', sans-serif",
+  },
+  {
+    label: "Libre Baskerville",
+    value: "''Libre Baskerville', serif",
+  },
+  {
+    label: "Libre Caslon Text",
+    value: "'Libre Caslon Text', serif",
+  },
+  {
+    label: "Martian Mono",
+    value: "'Martian Mono', monospace",
+  },
+  {
+    label: "Merriweather",
+    value: "'Merriweather', serif",
+  },
+  {
+    label: "Montserrat",
+    value: "'Montserrat', sans-serif",
+  },
+  {
+    label: "Nunito",
+    value: "'Nunito', sans-serif;",
+  },
+  {
+    label: "Open Sans",
+    value: "'Open Sans', sans-serif",
+  },
+  {
+    label: "PT Serif",
+    value: "'PT Serif', serif",
+  },
+  {
+    label: "PT Serif",
+    value: "'PT Serif', serif",
+  },
+];
